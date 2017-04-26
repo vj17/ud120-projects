b63aa89226bf159e082d7ef28f8d474287ce5ac1
root <root@nva-kvm-43.englab.brocade.com>
2017-04-26 16:00:33 +0530

    1. Add code to shorten the training data set 2. Add code to print only certain labels 3. Use kernel=rbf and C=10000 for an Optimized model(99%)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,22 +19,31 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf",C=10000)
 
 t0 = time()
 clf.fit(features_train,labels_train)
 print "training time:",round(time()-t0,3),"s"
 
 t1 = time()
-clf.predict(features_test)
+pred = clf.predict(features_test)
 print "testing time:",round(time()-t1,3),"s"
+print "label for 10:",pred[10]
+print "label for 26:",pred[26]
+print "label for 50:",pred[50]
 
+print "No. of points predicted to be in Chris class:"
+count = 0
+for i in pred:
+    if i == 1:
+	count+=1
+
+print count
 print "accuracy:",clf.score(features_test,labels_test)
 #########################################################
 
