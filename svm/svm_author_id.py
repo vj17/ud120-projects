5a95c90ee285a27a8057bcdf6305dcfeb36abe25
root <root@nva-kvm-43.englab.brocade.com>
2017-04-26 15:24:00 +0530

    Use SVM algorithm on Terrain data set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.svm import SVC
+clf = SVC(kernel="linear")
+clf.fit(features_train,labels_train)
+clf.predict(features_test)
+print "accuracy:",clf.score(features_test,labels_test)
 #########################################################
 
 
