7ef4bada5104a101d7deb1c66abbc7d95a423dd5
root <root@nva-kvm-43.englab.brocade.com>
2017-05-18 13:15:38 +0530

    Hacks for outlier detection

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -66,6 +67,11 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+# Invert train and test data for outlier effects
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print "Slope for outlier detection:",reg.coef_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
