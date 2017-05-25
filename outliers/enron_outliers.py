5fb2fb9193a1e934ca931e9215581281a8c1deb0
root <root@nva-kvm-43.englab.brocade.com>
2017-05-25 12:11:16 +0530

    Identify and remove Enron outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,10 +10,32 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+#Remove TOTAL outlier
+data_dict.pop("TOTAL",0)
+
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
+"""max_salary = max(data[:,0])
+for k,v in data_dict.items():
+    if(v["salary"] == max_salary):
+	print "Max salary",k
+"""
 
+#Print the max 2 outliers
+max_salaries =  sorted(data[:,0],reverse=True)[:2]
+print max_salaries
+for k,v in data_dict.items():
+    if v["salary"] in max_salaries:
+	print k
